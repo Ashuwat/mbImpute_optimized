3b6fbafa3e9c18e550b50f627b841466b93c6996
Ashuwat <150308091+Ashuwat@users.noreply.github.com>
2025-03-16 21:05:34 -0400

    Update data_fit2.R

--- mbImpute R package/R/data_fit2.R
+++ mbImpute R package/R/data_fit2.R
@@ -223,7 +223,7 @@ data_fit2 <- function(y_sim, metadata, D, k, parallel = F, ncores = 1){
     # pz = 1 - nz/n
     # test = pz - 1.96 * sqrt(pz * (1-pz)/n)
     # if(nz == n || test <= 0){
-    if(sum(y <= (log10(1.01) + 1e-6)) / n > 0.88){
+    if(sum(y <= (log10(1.01) + 1e-6)) / n > 0.89){
       return(false)
     }else{
       return(true)
